4a2d3ed1ed02fd7850a3fd83b523dd4bafee31dc
Issac <ichaudr1@umbc.edu>
2023-04-11 08:55:57 -0400

    updates for changes in Entrez api

--- genome_fragment.py
+++ genome_fragment.py
@@ -142,8 +142,9 @@ class GenomeFragment:
 
                 try:
 
-                    handle = Entrez.efetch(db="nuccore", id=self.genome_accession, strand=1, seq_start='begin', seq_stop='end', rettype='gbwithparts', retmode='XML')
+                    handle = Entrez.efetch(db="nuccore", id=self.genome_accession, strand=1, seq_start='begin', seq_stop='end', rettype='gb', retmode='xml')
                     record = handle.read()
+                    print(record)
                     time.sleep(self.sleep_time)
 
                     break
